7f2fb199c920cb37efab8b9374f95a99cb42eabe
vungocanh2k2 <90661733+vungocanh2k2@users.noreply.github.com>
2022-06-13 20:40:49 +0700

    sua index

---
+++ Resources/js/test.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    
+    "use strict";
+        
+        $('h1').click(function () {
+            $(this).css('background-color', '#ff0000');
+        });
+        
+    });
